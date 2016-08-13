95d0a2a08657d2e5b7842c7ecf747da850eae783
Artem Russkikh <ra@itbeaver.co>
2016-08-13 12:44:27 +0500

    Rolling not by zero points

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 describe BowlingGame do
-  it('can be rolled') { is_expected.to respond_to :roll }
-  it('can get score') { is_expected.to respond_to :score }
+  it('can be #roll') { is_expected.to respond_to :roll }
+  it('can get #score') { is_expected.to respond_to :score }
 
-  context 'rolled 20.times' do
+  context '#roll 20.times with zero pins' do
     before { 20.times { subject.roll(pins: 0) } }
 
-    it('score is zero') { expect(subject.score).to eq(0) }
+    it('#score is correct') { expect(subject.score).to eq(0) }
+  end
+
+  context '#roll 20.times with one pins' do
+    before { 20.times { subject.roll(pins: 1) } }
+
+    it('#score is correct') { expect(subject.score).to eq(20) }
   end
 end
