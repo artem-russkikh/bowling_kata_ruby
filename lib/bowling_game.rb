848b3a9c26683f91e84a03f845d226999dfe632e
Artem Russkikh <ra@itbeaver.co>
2016-08-13 20:40:19 +0500

    Spec pass

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'active_support/core_ext/array/grouping'
+require 'active_support/core_ext/object/try'
 require 'bowling_game/frame'
-require 'bowling_game/pin'
 
 # A game has 10 frames
 # A frame has 1 or two rolls
@@ -9,63 +9,85 @@ require 'bowling_game/pin'
 # It is different from all the other frames
 class BowlingGame
   def initialize
-    @rolls = []
-    @current_roll = 0
+    @frames = []
+    @current_frame = 0
   end
 
   # Called each time the player rolls a ball
   # @param pins [Integer] the number of pins knocked down
   # @return true
   def roll(pins:)
-    @rolls[current_roll] = pins
-    @current_roll = current_roll + 1
-    roll(pins: 0) if strike?(pins)
+    return false if current_frame >= 10
+    updated_frame = frame_after_roll(pins)
+    if updated_frame
+      @frames[current_frame] = updated_frame
+    else
+      @current_frame = current_frame + 1
+      @frames[current_frame] = Frame.new([pins])
+    end
     true
+  end
+
+  # Return proceeded frame after performing roll with pins
+  # @return [BowlingGame::Frame]
+  def frame_after_roll(pins)
+    frame = @frames[current_frame].try(:dup)
+    return Frame.new([pins]) if frame.nil?
+    roll_limit = 2
+    roll_limit = 1 if frame.pins.first == 10
+    roll_limit = 3 if current_frame == 10 && pins == 10
+    return nil if frame.count >= roll_limit
+    frame.push(pins)
+    frame
   end
 
   # Iterate through all the frames, and calculate all their scores
   # @return [Integer]
   def score
     result = 0
-    rolls.in_groups_of(2).each_with_index do |frame_array, frame_index|
-      result += process_frame(frame_array, frame_index)
+    frames.each_with_index do |frame, frame_index|
+      result += process_frame(frame, frame_index)
     end
     result
   end
 
   private
 
-  # @return [Boolean]
-  def strike?(pins)
-    pins == 10
-  end
-
   # @return [Integer]
   def strike_bonus(frame_index)
-    next_frame = Frame.new(rolls.in_groups_of(2)[frame_index + 1])
-    next_frame.score
+    next_frame = frames[frame_index + 1]
+    return 0 unless next_frame
+    if next_frame.strike?
+      frame_after_next = frames[frame_index + 2]
+      if frame_after_next
+        next_frame.score + frame_after_next.score
+      else
+        next_frame.score
+      end
+    else
+      next_frame.score
+    end
   end
 
   # @return [Integer]
   def spare_bonus(frame_index)
-    next_frame = Frame.new(rolls.in_groups_of(2)[frame_index + 1])
+    next_frame = frames[frame_index + 1]
     next_frame.first_roll
   end
 
   # @return [Integer]
-  def process_frame(frame_array, frame_index)
-    current_frame = Frame.new(frame_array)
-    if current_frame.strike?
-      current_frame.score + strike_bonus(frame_index)
-    elsif current_frame.spare?
-      current_frame.score + spare_bonus(frame_index)
+  def process_frame(frame, frame_index)
+    if frame.strike?
+      frame.score + strike_bonus(frame_index)
+    elsif frame.spare?
+      frame.score + spare_bonus(frame_index)
     else
-      current_frame.score
+      frame.score
     end
   end
 
   # @return [Array]
-  attr_reader :rolls
+  attr_reader :frames
   # @return [Integer]
-  attr_reader :current_roll
+  attr_reader :current_frame
 end
--- lib/bowling_game/frame.rb
+++ lib/bowling_game/frame.rb
@@ -1,40 +1,52 @@
 # frozen_string_literal: true
 require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/enumerable'
+require 'active_support/core_ext/module/delegation'
 
 class BowlingGame
   # Class for safely accessing score in frames
   class Frame
-    # @return [Integer]
-    attr_reader :first_roll
-    # @return [Integer]
-    attr_reader :second_roll
+    # @return [Array]
+    attr_reader :pins
+
+    delegate :count, :size, to: :pins
 
     # @return [BowlingGame::Frame]
-    def initialize(frame = [])
-      args = frame
-      args = [] unless args.is_a?(Array)
-      args = args.first(2).delete_if(&:blank?)
-      raise ArgumentError if args.any? { |roll| !roll.is_a?(Integer) }
-      @first_roll = args[0] || 0
-      @second_roll = args[1] || 0
+    def initialize(args = [])
+      @pins = args
+      @pins = [] unless pins.is_a?(Array)
+      @pins = pins.delete_if(&:blank?)
+      raise ArgumentError if pins.any? { |pin| !pin.is_a?(Integer) }
     end
 
-    # @note immutable
+    # @return [Integer]
+    def first_roll
+      pins[0] || 0
+    end
+
+    # @return [Integer]
+    def second_roll
+      pins[1] || 0
+    end
+
     # @return [Boolean]
     def strike?
-      @is_strike ||= first_roll == 10
+      first_roll == 10
     end
 
-    # @note immutable
     # @return [Boolean]
     def spare?
-      @is_spare ||= score == 10
+      score == 10
     end
 
-    # @note immutable
     # @return [Integer]
     def score
-      @score ||= first_roll + second_roll
+      pins.sum
+    end
+
+    # @return [BowlingGame::Frame]
+    def push(pin)
+      @pins.push(pin)
     end
   end
 end
