2d82b467009150454517d218e1ce48fbbe5c3264
Artem Russkikh <ra@itbeaver.co>
2016-08-13 12:37:15 +0500

    Expect score is number

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 describe BowlingGame do
-  it 'can be rolled' do
-    expect(subject).to respond_to :roll
-  end
+  it('can be rolled') { is_expected.to respond_to :roll }
+  it('can get score') { is_expected.to respond_to :score }
 
-  it 'can get score' do
-    expect(subject).to respond_to :score
+  context 'rolled 20.times' do
+    before { 20.times { subject.roll(0) } }
+
+    it('score is zero') { expect(subject.score).to eq(0) }
   end
 end
