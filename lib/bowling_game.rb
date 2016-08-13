9502b15b476d78efcc13a5dba93e80807e2be901
Artem Russkikh <ra@itbeaver.co>
2016-08-13 12:58:01 +0500

    Spec pass

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -5,17 +5,20 @@
 # The 'tenth frame' has two or three rolls
 # It is different from all the other frames
 class BowlingGame
+  # @see #score
+  attr_writer :score
+
   # Called each time the player rolls a ball
   # @param pins [Integer] the number of pins knocked down
   # @return true
-  # def roll(pins:)
-  def roll(*)
+  def roll(pins:)
+    self.score += pins
     true
   end
 
   # Iterate through all the frames, and calculate all their scores
   # @return [Integer]
   def score
-    0
+    @score || 0
   end
 end
