67322bf69cbc5542f8d2f53705db5e40c0fb2342
Artem Russkikh <ra@itbeaver.co>
2016-08-13 12:23:26 +0500

    Spec pass

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+# :nodoc:
 class BowlingGame
+  def roll
+  end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 describe BowlingGame do
   it 'can roll' do
     expect(subject).to respond_to :roll
